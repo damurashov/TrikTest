3c43fde329a4fa23028ae33f1775a20c2f80c7b0
DmitryMurashov <dmtr.murashov@gmail.com>
2022-03-24 19:20:57 +0300

    [:main] Int

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import server
-import trik
+import server, trik, client, trik_test
 
 
 if __name__ == "__main__":
 	server.start(trik.tcp_handle)
+	# client.async_connect("192.168.4.1", 8889, trik_test.handle)
+	client.async_connect("127.0.0.1", 3000, trik_test.handle_run_test_registry)
 	while True:
-		pass
+		command = input("> ")
+		trik_test.command(command)
