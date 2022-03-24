bf64f5159716514af7fd1b719d01abd49376e375
DmitryMurashov <dmtr.murashov@gmail.com>
2022-03-24 18:57:00 +0300

    [:client] Enh | Exception handling

--- client.py
+++ client.py
@@ -1,20 +1,28 @@
 import socket
 from threading import Thread
+from generic import Logging
+
 
 class _Client:
 	_threads = list()
 
 	@staticmethod
-	def _async_connect(host, port, handle=lambda conn, addr: None):
-		conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		conn.connect((host, port,))
-		handle(conn, (host, port,))
-		conn.shutdown(2)
-		conn.close()
-
-	@staticmethod
 	def async_connect(host, port, handle = lambda conn, addr: None):
-		_Client._threads.append(Thread(target=_Client._async_connect, args=(host, port, handle)))
+		def task():
+			conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+			try:
+				conn.connect((host, port,))
+				Logging.info(__file__, "created client connection", conn.getpeername())
+				handle(conn, (host, port,))
+				Logging.info(__file__, "shutting down client connection", conn.getpeername())
+				conn.shutdown(2)
+			except ConnectionRefusedError:
+				Logging.error(__file__, "could not connect to", host, port)
+			finally:
+				conn.close()
+
+		_Client._threads.append(Thread(target=task))
 		_Client._threads[-1].start()
 
 
