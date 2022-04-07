73351dcee06067e79d8a44cc0fb4b51f5fd82422
DmitryMurashov <dmtr.murashov@gmail.com>
2022-04-07 13:52:01 +0300

    [snippets:trik_client] Bug | Missing main() body

--- snippets/trik_client.js
+++ snippets/trik_client.js
@@ -1,12 +1,14 @@
 
-mailbox.connect("192.168.4.1", 8889);
+var main = function() {
+	mailbox.connect("192.168.4.1", 8889);
 
-for (let i = 0; i < 10; ++i) {
-	print("sending " + i);
-	mailbox.send(42, "echo " + i);
+	for (var i = 0; i < 10; ++i) {
+		print("sending " + i);
+		mailbox.send(42, "echo " + i);
 
-	print("receiving " + i);
-	message = mailbox.receive();
+		print("receiving " + i);
+		message = mailbox.receive();
 
-	print(message);
+		print(message);
+	}
 }
